8f67c5e7cd66597975c8b913a19724b0e7d52a40
irene macapinlac <irenekaymac@gmail.com>
2020-06-13 23:46:07 +1000

    added content to HTML and further styled on CSS

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,17 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search...">
       </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Sydney, Australia</div>
+          <div class="date">Sunday 14 June 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">17°c</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
     </main>
 
   </div>
@@ -68,6 +79,48 @@ main {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.57);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
 
 }
+
 </style>
